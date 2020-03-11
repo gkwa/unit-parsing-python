d219121dfe3f5c28dc89faae63eddc7627a6c3f9
mtmonacelli <taylormonacelli@gmail.com>
2020-03-10 18:04:21 -0700

    root handler is enough to show debug of prices module

--- o8.py
+++ o8.py
@@ -6,9 +6,6 @@ import sys
 from unitparsing_pkg.prices import (Bundle, CaculateUnitPriceException,
                                     ParseQuantityException, UnitPrice)
 
-l1 = logging.getLogger("unitparsing_pkg.prices")
-l1.setLevel(logging.DEBUG)
-
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
@@ -18,7 +15,6 @@ formatter = logging.Formatter("[%(asctime)s] p%(process)s {%(filename)s:%(lineno
 handler.setFormatter(formatter)
 
 root.addHandler(handler)
-l1.addHandler(handler)
 
 for key in logging.Logger.manager.loggerDict:
     print(key)
